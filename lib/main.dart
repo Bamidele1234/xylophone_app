5798e1877e3c9112d7985f283048523e85a20aa8
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-05-23 17:31:13 +0100

    Done !

--- lib/main.dart
+++ lib/main.dart
@@ -13,87 +13,39 @@ class MyApp extends StatelessWidget {
     player.play('note$playNumber.wav');
   }
 
-  final double height = 100;
+  Expanded buildKey(int sound, Color myColor) {
+    return Expanded(
+      child: Container(
+        color: myColor,
+        child: TextButton(
+          onPressed: () {
+            playSound(sound);
+          },
+          child: const Text(''),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-            //TextButton(onPressed: (){}, child: child)
-            Container(
-              height: height,
-              color: Colors.green,
-              child: TextButton(
-                onPressed: () {
-                  playSound(1);
-                },
-                child: const Text(''),
-              ),
-            ),
-            Container(
-              height: height,
-              color: Colors.pink,
-              child: TextButton(
-                onPressed: () {
-                  playSound(2);
-                },
-                child: const Text(''),
-              ),
-            ),
-            Container(
-              height: height,
-              color: Colors.brown,
-              child: TextButton(
-                onPressed: () {
-                  playSound(3);
-                },
-                child: const Text(''),
-              ),
-            ),
-            Container(
-              height: height,
-              color: Colors.yellow,
-              child: TextButton(
-                onPressed: () {
-                  playSound(4);
-                },
-                child: const Text(''),
-              ),
-            ),
-            Container(
-              height: height,
-              color: Colors.lightBlue,
-              child: TextButton(
-                onPressed: () {
-                  playSound(5);
-                },
-                child: const Text(''),
-              ),
-            ),
-            Container(
-              height: height,
-              color: Colors.deepPurple,
-              child: TextButton(
-                onPressed: () {
-                  playSound(6);
-                },
-                child: const Text(''),
-              ),
-            ),
-            Container(
-              height: height,
-              color: Colors.deepOrangeAccent,
-              child: TextButton(
-                onPressed: () {
-                  playSound(7);
-                },
-                child: const Text(''),
-              ),
-            ),
-          ]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.blue),
+              buildKey(6, Colors.indigo),
+              buildKey(7, Colors.purple),
+            ],
+          ),
         ),
       ),
     );
